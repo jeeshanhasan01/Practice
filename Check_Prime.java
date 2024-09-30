0b7853a942972a2156e1adb50e9eb99f1e2808c6
jeeshanhasan01 <jeeshanhasan786@gmail.com>
2024-09-30 23:13:43 +0530

    Create Check_Prime.java
    Check Prime Number

---
+++ Check_Prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Check_Prime {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int count=0;
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count>=1){
+            System.out.println("not prime");
+        }
+        else{
+            System.out.println("prime");
+        }
+        
+    }
+}
