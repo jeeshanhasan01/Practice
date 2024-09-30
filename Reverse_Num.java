0331af731b728de5ebd1df5dbde4e468757224dc
jeeshanhasan01 <jeeshanhasan786@gmail.com>
2024-10-01 00:19:05 +0530

    Create Reverse_Num.java

---
+++ Reverse_Num.java
@@ -0,0 +1,15 @@
+public class Reverse_Num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 642;
+		int rev = 0;
+		while (n > 0) {
+			int rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n / 10;
+		}
+		System.out.println(rev);
+	}
+
+}
